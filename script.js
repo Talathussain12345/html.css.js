afecb2294dc75f1000abda1f32fc6ae8372670be
Talathussain12345 <49521244+Talathussain12345@users.noreply.github.com>
2019-06-03 02:22:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function showAlert()
+{ 
+    alert('hero no 1') ;
+    alert('hero no 2') ;
+    alert('hero no 3') ;
+    alert('hero no 4') ;
+    alert('hero no 5') ;
+    alert('hero no 6') ;
+    alert('hero no 7') ;
+    alert('hero no 8') ;
+    alert('hero no 0') ;
+}
+
+function onhover()
+{
+    console.log('pakistani 1');
+    console.log('pakistani 0');
+    console.log('pakistani 1');
+    console.log('pakistani 0');
+    console.log('pakistani 1');
+    console.log('pakistani 0');
+    console.log('pakistani 1');
+    console.log('pakistani 0');   
+}
+
+
